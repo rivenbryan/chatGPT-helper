e9d8e6232a84a40f71415a487b8db496e2aef149
Bryan Tay <rivenbryan@gmail.com>
2023-03-25 01:44:15 +0800

    linked up both frontend and backend

--- chatgpt-parameters[BE]/controller/chatGPTController.js
+++ chatgpt-parameters[BE]/controller/chatGPTController.js
@@ -2,15 +2,27 @@
 const axios = require('axios');
 
 const sendChatMessage = async (req ,res) => {
-    const body = req.body
+    const messageDictionary = req.body
+    messageDictionary['role'] = 'user';
+
+    const chatAPIcontent = { 
+        "model": "gpt-3.5-turbo", 
+        "messages": [ 
+            messageDictionary, 
+            
+            ] 
+    }
+    
     try {
-        const response = await axios.post('https://chatgpt-api.shn.hk/v1/', body, {
+        const response = await axios.post('https://chatgpt-api.shn.hk/v1/', chatAPIcontent, {
             headers: {
               'Content-Type': 'application/json'
             }
           });
         const message = response.data.choices[0].message.content
-        res.send(message)
+        console.log(message)
+
+        res.send({text: "5", component: "comment"})
     } catch (error) {
         console.log(error)
         res.status(500).send('Something went wrong!');
--- chatgpt-parameters[FE]/src/App.tsx
+++ chatgpt-parameters[FE]/src/App.tsx
@@ -1,32 +1,40 @@
 import { TextInput, Checkbox, Button, Group, Box, MantineProvider, Text } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function App() {
+  const [message, setMessage] = useState("")
+
   const form = useForm({
     initialValues: {
-      email: '',
-      termsOfService: false,
+      content: '',
     }
   });
-//test
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log("Clicked!")
+    const content = form.values;
+    axios.post('http://localhost:5000/api/request', content)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => console.error(error));
+  }
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+      <Text>Welcome to ChatGPT</Text>
       <Box maw={300} mx="auto">
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <form onSubmit={handleSubmit}>
           <TextInput
             withAsterisk
             label="Email"
             placeholder="your@email.com"
-            {...form.getInputProps('email')}
+            {...form.getInputProps('content')}
           />
-
-          <Checkbox
-            mt="md"
-            label="I agree to sell my privacy"
-            {...form.getInputProps('termsOfService', { type: 'checkbox' })}
-          />
-
+            {message}
           <Group position="right" mt="md">
             <Button type="submit">Submit</Button>
           </Group>
